7e2c36116957131519478c4f69df71df8ab0f8cd
Mike Walters <mike@eventbooking.com>
2016-12-21 13:26:41 -0500

    close button

--- dist/angular-typeahead.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="../typings/tsd.d.ts" />
-/// <reference path="../bower_components/angular-typescript-module/dist/angular-typescript-module.d.ts" />
-declare var Tether: any;
-declare module AngularTypeaheadModule {
-}
-declare module AngularTypeaheadModule {
-}
