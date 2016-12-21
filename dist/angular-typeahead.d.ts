4877c9e7b06f9e1406aa9d1a3ed09f068d602cc3
Mike Walters <mike@eventbooking.com>
2016-12-21 13:36:37 -0500

    fixed package

---
+++ dist/angular-typeahead.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="../typings/tsd.d.ts" />
+/// <reference path="../bower_components/angular-typescript-module/dist/angular-typescript-module.d.ts" />
+declare var Tether: any;
+declare module AngularTypeaheadModule {
+}
+declare module AngularTypeaheadModule {
+}
